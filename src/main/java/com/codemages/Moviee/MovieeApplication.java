0fefb74afcf5a481a5147f3dd1af698f7d94aa2f
Wesley Prado <wesleyprado.dev@gmail.com>
2024-11-24 17:50:02 -0300

    refactor: remove unnecessary blank lines in MovieeApplication.java

--- src/main/java/com/codemages/Moviee/MovieeApplication.java
+++ src/main/java/com/codemages/Moviee/MovieeApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MovieeApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MovieeApplication.class, args);
 	}
-
 }
